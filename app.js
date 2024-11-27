984f099c9364e0d329a2424db0a1e7a856480a20
hadiheidariazar <hadiha010101@gmail.com>
2024-11-27 17:52:45 +0330

    finish coding show winner modal

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const weaponPlayer = document.querySelector('.player .weapon p')
 const weaponRobot = document.querySelector('.robot .weapon p')
 const playerScoreSpan = document.querySelector('.player-score')
 const robotScoreSpan = document.querySelector('.robot-score')
+const modal = document.querySelector('.modal')
+const titleModal = document.querySelector('.title-modal')
+const closeModalBtn = document.querySelector('.modal-container button')
 
 const weapons = [
     { title: '✋', lose: '✌' },
@@ -15,16 +18,38 @@ let robotScore = 0
 let playerScore = 0
 
 const selectWeaponHandle = (button) => {
-    let robotWeapon = weapons[Math.ceil(Math.random() * weapons.length) - 1]
+    if (robotScore < finallScore && playerScore < finallScore) {
+        let robotWeapon = weapons[Math.ceil(Math.random() * weapons.length) - 1]
 
-    weaponPlayer.innerHTML = ''
-    weaponPlayer.innerHTML = button.value;
-    weaponRobot.innerHTML = robotWeapon.title;
+        weaponPlayer.innerHTML = ''
+        weaponPlayer.innerHTML = button.value;
+        weaponRobot.innerHTML = robotWeapon.title;
 
-    (robotWeapon.lose === button.value || robotWeapon.title === button.value) ? (robotWeapon.lose === button.value) && (playerScore += 1) : (robotScore += 1);
+        (robotWeapon.lose === button.value || robotWeapon.title === button.value) ? (robotWeapon.lose === button.value) && (playerScore += 1) : (robotScore += 1);
 
+        updateScoresInDom(playerScore, robotScore)
+
+        if (robotScore === finallScore || playerScore === finallScore) {
+            modal.classList.add('show')
+            titleModal.innerHTML = robotScore === finallScore ? "باختی 😢" : "برنده شدی 😎"
+
+            closeModalBtn.addEventListener('click', () => {
+                playerScore = 0
+                robotScore = 0
+                weaponPlayer.innerHTML = '؟'
+                weaponRobot.innerHTML = '؟'
+                updateScoresInDom(playerScore, robotScore)
+
+                modal.classList.remove('show')
+            })
+        }
+    }
+
+}
+
+const updateScoresInDom = (playerScore, robotScore) => {
     playerScoreSpan.innerHTML = playerScore
-    robotScoreSpan.innerHTML = robotScore
+    robotScoreSpan.innerHTML = robotScore;
 }
 
 weaponsButtons.forEach(button => button.addEventListener('click', () => selectWeaponHandle(button)))
